50e38242111eb4b7ef59af1d4852a14a49627bac
Chen <tychen2@fa19-cs427-101.cs.illinois.edu>
2019-09-07 00:41:43 -0500

    MP1: Building with bash script

--- mp1/CoffeeMaker_Web/build.sh
+++ mp1/CoffeeMaker_Web/build.sh
@@ -2,8 +2,8 @@ mkdir -p target/CoffeeMaker_Web/WEB-INF/classes/
 javac src/main/java/edu/ncsu/csc326/coffeemaker/exceptions/*.java
 javac -classpath src/main/java/ src/main/java/edu/ncsu/csc326/coffeemaker/*.java
 mkdir -p target/CoffeeMaker_Web/WEB-INF/classes/edu/ncsu/csc326/coffeemaker/exceptions
-cp src/main/java/edu/ncsu/csc326/coffeemaker/*.java target/CoffeeMaker_Web/WEB-INF/classes/edu/ncsu/csc326/coffeemaker/
-cp src/main/java/edu/ncsu/csc326/coffeemaker/exceptions/*.java target/CoffeeMaker_Web/WEB-INF/classes/edu/ncsu/csc326/coffeemaker/exceptions/
+cp src/main/java/edu/ncsu/csc326/coffeemaker/*.class target/CoffeeMaker_Web/WEB-INF/classes/edu/ncsu/csc326/coffeemaker/
+cp src/main/java/edu/ncsu/csc326/coffeemaker/exceptions/*.class target/CoffeeMaker_Web/WEB-INF/classes/edu/ncsu/csc326/coffeemaker/exceptions/
 cp -r src/main/webapp/*.jsp target/CoffeeMaker_Web/
 cp src/main/webapp/WEB-INF/web.xml target/CoffeeMaker_Web/WEB-INF/
 cd target/CoffeeMaker_Web/
